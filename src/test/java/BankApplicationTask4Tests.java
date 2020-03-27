b17a5e669c07a49564258628ee333b5be7752bd1
oyushche <uhmateush@gmail.com>
2020-03-27 02:56:43 -0400

    task 4 test final

--- src/test/java/BankApplicationTask4Tests.java
+++ src/test/java/BankApplicationTask4Tests.java
@@ -1,7 +1,4 @@
-import com.luxoft.bankapp.model.Account;
-import com.luxoft.bankapp.model.AccountType;
-import com.luxoft.bankapp.model.CheckingAccount;
-import com.luxoft.bankapp.model.Client;
+import com.luxoft.bankapp.model.*;
 import com.luxoft.bankapp.service.Banking;
 import com.luxoft.bankapp.service.BankingImpl;
 import com.luxoft.bankapp.service.storage.ClientStorage;
@@ -10,7 +7,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.PropertySource;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
@@ -33,6 +33,29 @@ public class BankApplicationTask4Tests
     @Autowired
     private Storage storage;
 
+    @Autowired
+    @Qualifier("savingAccount1")
+    private SavingAccount savingAccount1;
+
+    @Autowired
+    @Qualifier("checkingAccount1")
+    private CheckingAccount checkingAccount1;
+
+    @Autowired
+    @Qualifier("client1")
+    private Client client1;
+
+    @Autowired
+    @Qualifier("checkingAccount2")
+    private CheckingAccount checkingAccount2;
+
+    @Autowired
+    @Qualifier("client2")
+    private Client client2;
+
+    @Autowired
+    private PropertySourcesPlaceholderConfigurer placeholderConfigurer;
+
     @BeforeEach
     public void init()
     {
@@ -50,6 +73,18 @@ public class BankApplicationTask4Tests
     }
 
     @Test
+    public void placeholderConfigurerBeanConfiguration()
+    {
+        assertNotNull(placeholderConfigurer, "placeholderConfigurer bean should be configured");
+
+        PropertySource<?> localProperties = placeholderConfigurer.getAppliedPropertySources().get("localProperties");
+        assertNotNull(localProperties, "You should configure PropertySourcesPlaceholderConfigurer bean");
+
+        assertEquals("Jonny Bravo", localProperties.getProperty("client1"));
+        assertEquals("Adam Budzinski", localProperties.getProperty("client2"));
+    }
+
+    @Test
     public void storageBeanConfiguration()
     {
         assertNotNull(storage, "storage bean should be configured");
@@ -61,6 +96,52 @@ public class BankApplicationTask4Tests
     {
         assertNotNull(banking, "banking bean should be configured");
         assertTrue((banking instanceof BankingImpl), "storage should be instantiated with BankingImpl class");
+    }
+
+    @Test
+    public void savingAccount1BeanConfiguration()
+    {
+        assertNotNull(savingAccount1, "savingAccount1 bean should be configured");
+        assertTrue((savingAccount1 instanceof SavingAccount), "savingAccount1 should be instantiated with SavingAccount class");
+    }
+
+    @Test
+    public void checkingAccount1BeanConfiguration()
+    {
+        assertNotNull(checkingAccount1, "checkingAccount1 bean should be configured");
+        assertTrue((checkingAccount1 instanceof CheckingAccount), "checkingAccount1 should be instantiated with CheckingAccount class");
+    }
+
+    @Test
+    public void client1Configuration()
+    {
+        assertNotNull(client1, "client1 bean should be configured");
+        assertTrue((client1 instanceof Client), "client1 should be instantiated with Client class");
+
+        assertSame(savingAccount1, client1.getAccount(AccountType.SAVING));
+        assertSame(checkingAccount1, client1.getAccount(AccountType.CHECKING));
+
+        assertEquals(Client.Gender.MALE, client1.getGender());
+        assertEquals("Moscow", client1.getCity());
+    }
+
+    @Test
+    public void checkingAccount2BeanConfiguration()
+    {
+        assertNotNull(checkingAccount2, "checkingAccount2 bean should be configured");
+        assertTrue((checkingAccount2 instanceof CheckingAccount), "checkingAccount2 should be instantiated with CheckingAccount class");
+    }
+
+    @Test
+    public void client2Configuration()
+    {
+        assertNotNull(client2, "client2 bean should be configured");
+        assertTrue((client2 instanceof Client), "client2 should be instantiated with Client class");
+
+        assertSame(checkingAccount2, client2.getAccount(AccountType.CHECKING));
+
+        assertEquals(Client.Gender.MALE, client2.getGender());
+        assertEquals("Kiev", client2.getCity());
     }
 
     @Test

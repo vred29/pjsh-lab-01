9475d188b337907fdcea8ce3f2fbbc5e66e68096
oyushche <uhmateush@gmail.com>
2020-04-27 04:55:03 -0400

    useless banking bean removed

--- src/main/java/com/luxoft/bankapp/service/demo/DemoBankInitializationService.java
+++ src/main/java/com/luxoft/bankapp/service/demo/DemoBankInitializationService.java
@@ -1,7 +1,5 @@
 package com.luxoft.bankapp.service.demo;
 
-import com.luxoft.bankapp.service.Banking;
-
 import com.luxoft.bankapp.service.feed.BankFeedServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
@@ -17,26 +15,12 @@ public class DemoBankInitializationService implements BankInitializationService
     public static final String DEMO_FEED_FILE_NAME = "demo.feed";
 
     @Autowired
-    private Banking banking;
-
-    @Autowired
     private BankFeedServiceImpl feedService;
 
     @Override
+    @PostConstruct
     public void createClientsForDemo()
     {
         feedService.loadFeed(new File(DEMO_FEED_FILE_NAME));
-    }
-
-    @PostConstruct
-    private void init()
-    {
-        feedService.setBanking(banking);
-        createClientsForDemo();
-    }
-
-    public Banking getBanking()
-    {
-        return banking;
     }
 }
